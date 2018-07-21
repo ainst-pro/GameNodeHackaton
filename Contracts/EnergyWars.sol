096376d99918c5a424e1b01b443fce50aa1f4293
andrey <smyaksmyak@gmail.com>
2018-07-21 15:59:47 +0300

    Smart contract implementation

--- Contracts/EnergyWars.sol
+++ Contracts/EnergyWars.sol
@@ -6,44 +6,72 @@ contract EnergyWars {
     struct Bonus {
         uint16 position;
         uint16 value;
-        bool isView;
     }
 
-    struct Gamer {
-        address idx;
+    struct Player {
+        address playerAddress;
         uint16 position;
         uint256 energy;
     }
 
-    uint16[900] gamePlace;
+    uint8[900] field;
 
-    Gamer[] gamers;
+    Player[] public players;
     mapping (address => boolean) checkGamer;
-    uint8 idxGamerStep = 0;
+    uint8 public idxCurrentPlayerTurn = 0;
     Bonus public gameBonus;
+    enum GameState
+    {
+        WaitingForPlayers,
+        Started,
+        Finished
+    }
+
+    GameState public state = GameState.WaitingForPlayers;
 
     function EnergyWars(){
 
     }
 
-    function regGamer() public
+
+    function registerPlayer() public
     {
-        assert(gamer.length < 3);
+        assert(players.length < 3);
         assert(!checkGamer[msg.sender]);
 
-        uint16 position = uint16((blockhash(block.number) + block.timestamp) % 900);
-        gamers[msg.sender] = Gamer(position, 1);
+        // uint16 position = uint16((blockhash(block.number) + block.timestamp) % 900);
+        uint16 position;
+        if (players.length == 0) 
+            position = 94;
+        else if (players.length == 1) 
+            position = 147;
+        else
+            position = 796;
+
+        players.push(Player(msg.sender, position, 1 /*basic energy*/));
         checkGamer[msg.sender] = True;
+
+        if (players.length == 3) state = GameState.Started;
     }
 
-    function getInfoGamer(uint8 index) constant public returns(uint16 x, uint16 y, uint256 energy)
-    {
+    // gameBonus
 
+    function getGameField () constant public returns(uint8[900]) {
+            return field;
     }
 
-    function action() public
+    function action(int16 xOffset, int16 yOffset, uint8 indexTargetPlayer) public
     {
-        assert(gamers[idxGameStep].idx == msg.sender);
-        assert(gamer.length == 3);
+        Player memory player = players[idxCurrentPlayerTurn];
+        assert(player.playerAddress == msg.sender);
+        assert(state == GameState.Started);
+        // Ходим максимум на 3 в одном из направлений
+        asset(xOffset <= 3 && yOffset == 0  || yOffset <= 3 && xOffset == 0);
+        int16 x = int16(player.position) % 30;
+        int16 y = int16(player.position) / 30;
+        uint16 newX = x + xOffset;
+        uint16 newY = y + yOffset;
+        assert(newX > 0 && newX < 30);
+        assert(newY > 0 && newY < 30);
     }
 }
